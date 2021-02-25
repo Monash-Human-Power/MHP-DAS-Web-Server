26d5e99c86983af6245539004b575d2ad244fa2b
Kunj Dave <kdav0006@student.monash.edu>
2021-02-25 21:19:47 +1100

    Extract function to deal with sending config to boost

--- client/src/api/v3/boost.ts
+++ client/src/api/v3/boost.ts
@@ -1,6 +1,19 @@
 import { emit } from 'api/common/socket';
 import { BoostConfigType } from 'types/boost';
 
+type action = 'upload' | 'delete';
+
+function sendConfig(actionType: action, type: BoostConfigType, name: string, configContent: string | null | ArrayBuffer) {
+  const topic = 'send-config';
+  const payload = {
+    action: actionType,
+    configType: type,
+    configName: name,
+    content: configContent,
+  };
+  emit(topic, JSON.stringify(payload));
+}
+
 /**
  * Send the content of the given configuration file on `boost/configs/action` over MQTT
  *
@@ -11,20 +24,20 @@ export default function uploadConfig(
   type: BoostConfigType,
   configFile: File,
 ) {
-  const topic = 'send-config';
   const reader = new FileReader();
 
   // Called when FileReader has completed reading a file
   reader.onload = () => {
     console.log(reader.result);
-    const payload = {
-      action: 'upload',
-      configType: type,
-      configName: configFile.name,
-      content: reader.result,
-    };
-    emit(topic, JSON.stringify(payload));
+    if (type == 'all' && reader.result != null && typeof reader.result === 'string') {
+      const allConfigs = JSON.parse(reader.result);
+      for (const [key, ] of Object.entries(allConfigs)) {
+        sendConfig('upload', <BoostConfigType>key, allConfigs[key]['name'], null);
+      };
+    }
+    else {
+    sendConfig('upload', type, configFile.name, reader.result);
   };
-
   reader.readAsText(configFile);
-}
+  }
+};
